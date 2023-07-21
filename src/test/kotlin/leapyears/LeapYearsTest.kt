cdbf222d679517bfda3c75f7490ee039d1aa2af7
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2023-07-21 11:36:26 +0200

    Categorize years divisible by 4 as leap years

--- src/test/kotlin/leapyears/LeapYearsTest.kt
+++ src/test/kotlin/leapyears/LeapYearsTest.kt
@@ -1,6 +1,7 @@
 package leapyears
 
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class LeapYearsTest {
@@ -9,6 +10,10 @@ class LeapYearsTest {
         assertFalse(isLeapYear(2001))
     }
 
+    @Test fun `categorizes year divisible by 4 as a leap year`() {
+        assertTrue(isLeapYear(1996))
+    }
+
     private fun isLeapYear(year: Int): Boolean =
-        false
+        year % 4 == 0
 }
