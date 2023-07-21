b5630992b4c886c8486fce7bb083eabae315a583
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2023-07-21 11:59:49 +0200

    Inline method

--- src/test/kotlin/leapyears/LeapYearsTest.kt
+++ src/test/kotlin/leapyears/LeapYearsTest.kt
@@ -7,23 +7,20 @@ import org.junit.jupiter.api.Test
 class LeapYearsTest {
 
     @Test fun `does not categorize typical common year as a leap year`() {
-        assertFalse(isLeapYear(2001))
+        assertFalse(Year(2001).isLeap())
     }
 
     @Test fun `categorizes year divisible by 4 as a leap year`() {
-        assertTrue(isLeapYear(1996))
+        assertTrue(Year(1996).isLeap())
     }
 
     @Test fun `does not categorize year divisible by 4 and 100 as a leap year`() {
-        assertFalse(isLeapYear(1900))
+        assertFalse(Year(1900).isLeap())
     }
 
     @Test fun `categorizes year divisible by 4, 100 and 400 as a leap year`() {
-        assertTrue(isLeapYear(2000))
+        assertTrue(Year(2000).isLeap())
     }
-
-    private fun isLeapYear(year: Int): Boolean =
-        Year(year).isLeap()
 }
 
 class Year(private val number: Int) {
